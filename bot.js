43b7f67e4fb08c06f3ac4ec54840657174da03c2
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-19 15:55:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const char = {}
+const fs = require('fs');
+let userdata = JSON.parse(fs.readFileAsync('userData.json','utf8');
 client.on('ready', () => {
     console.log('I am ready!');
-	char["Ok"] = "Hey Buddy"
 });
 
 client.on('message', message => {
 	if (!message.channel.name === "рп") return;
-	message.delete()
-	if (message.content.substring(0,5) == ":test") {
-		message.channel.send("${char["Ok"]} lol")
+	var kar = false
+	if (message.content.startsWith(':char')) {
+		if (!userdata[message.author.id + message.guild.id]) userdata[message.author.id + message.guild.id] = {};
+		userdata[message.author.id + message.guild.id] = message.content.substring(5);
+		kar = true
+	};
+	if (message.content.startsWith(':me')) {
+		message.channel.send(message.content.substring
+		
+	if (!message.channel.name === "рп") return;
+	message.delete();
+	if (!userdata[message.author.id + message.guild.id]) userdata[message.author.id + message.guild.id] = {}
+	if (!userdata[message.author.id + message.guild.id].char) userdata[message.author.id] + message.guild.id].char = 'NoOne'
+	if (kar === false) {
+		message.channel.send(userdata[message.author.id + message.guild.id] + ":${message.content}")
 	}
 });
 
