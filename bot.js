c38a3b73d6b3a95bcffa8a3b6c003d95cf81f347
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-19 10:18:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const char = {}
 client.on('ready', () => {
     console.log('I am ready!');
+	char["Ok"] = "Hey Buddy"
 });
 
-if (message.author.bot === true) return;
-	if (message.content === ";verify") {
-		message.delete()
-		if (!message.channel.name === "потверждение") return;
-		message.channel.send('Потверждаю...').then(msg => {
-			msg.delete(6)
-		})
-		let sor = message.guild.roles.find('name','непотвержден')
-		let ser = message.guild.roles.find('name','Игрок')
-		message.guild.member(message.author).removeRole(sor)
-		message.guild.member(message.author).addRole(ser).then(() => {
-			message.channel.send('Потверждены!').then(msg => {
-				msg.delete(4)
-			})
-		} ).catch(err => {
-			message.channel.send('Че-то пошло не так').then(msg => {
-				msg.delete(4)
-			})
-		})
-	}
-});
+client.on('message', message => {
+	if (!message.channel.name === "рп") return;
+	message.delete()
+	if (message.content.substring(0,5) == "test") {
+		message.channel.send("${char["Ok"]} lol"
+};
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
