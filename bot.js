02850efb2f7f2cadb483bcf95f72a3bd7577e9d9
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-19 16:27:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,19 +7,21 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (!message.channel.name === "рп") return;
-	message.delete()
 	if (!ida[message.author.id]) ida[message.author.id] = {}
 	if (message.content.startsWith(':char')) {
 		ida[message.author.id].char = message.content.substring(5)
+		message.delete()
 		return
 	};
 	if (message.content.startsWith(':me')) {
 		message.channel.send(ida[message.author.id].char + " " + message.content.substring(4))
+		message.delete()
 		return
 	};
 	if (!ida[message.author.id].char) return;
-	message.channel.send(ida[message.author.id].char + ": " + message.content)
-	return
+	message.channel.send(ida[message.author.id].char + ": " + message.content).then(() => {
+		message.delete()
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
